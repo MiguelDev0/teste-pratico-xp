4531606008a13da3ae5e2d03f49916e8d28799ed
Miguel Coutinho <miguelcoutinho181@gmail.com>
2023-08-18 23:35:00 -0300

    configurando containers

--- src/XP.API/Controllers/WeatherForecastController.cs
+++ src/XP.API/Controllers/WeatherForecastController.cs
@@ -6,28 +6,10 @@ namespace XP.API.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string helloWorld()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Teste de fumaça";
         }
     }
 }
--- src/XP.API/Program.cs
+++ src/XP.API/Program.cs
@@ -6,13 +6,9 @@ namespace XP.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
 
